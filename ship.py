c700855d30f429a1adaafcaf802e1b9cc23d6def
liuz <3107014755@qq.com>
2025-07-25 08:43:10 +0800

    alien_invasion

--- ship.py
+++ ship.py
@@ -23,9 +23,9 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left >self.screen_rect.left:
             self.x -= self.settings.ship_speed
-        if self.moving_up and self.rect.top <self.screen_rect.top:
+        if self.moving_up and self.rect.top >self.screen_rect.top:
             self.y -= self.settings.ship_speed
-        if self.moving_down and self.rect.bottom >self.screen_rect.bottom:
+        if self.moving_down and self.rect.bottom <self.screen_rect.bottom:
             self.y += self.settings.ship_speed
 
         self.rect.x = self.x
